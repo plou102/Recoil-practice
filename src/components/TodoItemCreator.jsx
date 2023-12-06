5afcc5786525bc71b3346e0a0b370042ccc150f7
plou102 <plou102@naver.com>
2023-12-06 14:22:52 +0900

    Feat: todoList 추가 버튼 및 입력창 생성

--- src/components/TodoItemCreator.jsx
+++ src/components/TodoItemCreator.jsx
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { todoListState } from "../TodoList";
 
+let id = 0;
+function getId() {
+  return id++;
+}
+
 export default function TodoItemCreator() {
   const [inputValue, setInputValue] = useState("");
   const setTodoList = useSetRecoilState(todoListState);
 
+  function inputChange({ target: { value } }) {
+    setInputValue(value);
+  }
+
+  function addItem() {
+    setTodoList((oldList) => [
+      ...oldList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
+  }
+
   return (
     <>
-      <input type="text" value={inputValue} onChange={onChange} />
+      <input type="text" value={inputValue} onChange={inputChange} />
       <button onClick={addItem}>Add</button>
     </>
   );
